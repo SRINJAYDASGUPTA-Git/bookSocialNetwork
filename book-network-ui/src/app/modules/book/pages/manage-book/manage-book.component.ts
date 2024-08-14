a3fb8525fc88aef0ca313edaf9644d83061f11ae
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-08-15 00:45:11 +0530

    feat: Add manage-book and my-books components
    The commit adds the following components:
    - manage-book.component.html
    - manage-book.component.spec.ts
    - manage-book.component.ts
    - my-books.component.html
    - my-books.component.spec.ts
    - my-books.component.ts
    
    These components are part of the book module and provide functionality for managing books and displaying a user's books.

---
+++ book-network-ui/src/app/modules/book/pages/manage-book/manage-book.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-book',
+  templateUrl: './manage-book.component.html',
+  styleUrl: './manage-book.component.scss'
+})
+export class ManageBookComponent {
+
+}
