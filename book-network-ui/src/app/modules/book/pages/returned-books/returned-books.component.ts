b2f89700e25df0fe199c8bb55beb26369cc31e89
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-08-15 20:04:37 +0530

    feat: Add returned-books component
    A new component, `returned-books`, was added to the book module. This component displays a message indicating that the books have been returned.

---
+++ book-network-ui/src/app/modules/book/pages/returned-books/returned-books.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-returned-books',
+  templateUrl: './returned-books.component.html',
+  styleUrl: './returned-books.component.scss'
+})
+export class ReturnedBooksComponent {
+
+}
