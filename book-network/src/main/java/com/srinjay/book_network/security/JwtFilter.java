f9b64aa88c53e0359a85eb7b0397bae0f5036815
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-08-15 21:41:27 +0530

    Tried Docker

--- book-network/src/main/java/com/srinjay/book_network/security/JwtFilter.java
+++ book-network/src/main/java/com/srinjay/book_network/security/JwtFilter.java
@@ -12,12 +12,13 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
 
