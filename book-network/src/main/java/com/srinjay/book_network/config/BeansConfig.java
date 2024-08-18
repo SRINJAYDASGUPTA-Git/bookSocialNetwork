c6e48e620308b3908ed4663493698e17b7da6b0b
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-08-18 10:11:55 +0530

    CORS enabled for AWS EC2 instance IP AGAIN

--- book-network/src/main/java/com/srinjay/book_network/config/BeansConfig.java
+++ book-network/src/main/java/com/srinjay/book_network/config/BeansConfig.java
@@ -53,7 +53,7 @@ public class BeansConfig {
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials (true);
+        //config.setAllowCredentials (true);
         config.setAllowedOrigins (allowedOrigins);
         config.setAllowedHeaders (Arrays.asList ("*"));
         config.setAllowedMethods (Arrays.asList ("*"));
