4ecfdf85cead5df15a178debe70d29da39cd6466
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-08-20 11:54:50 +0530

    updated .gitignore

--- book-network-ui/src/environments/environment.development.ts
+++ book-network-ui/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production:false,
-  apiUrl: 'http://localhost:8080/api/v1'
+  apiUrl: 'http://localhost:8088/api/v1'
 };
--- book-network-ui/src/environments/environment.ts
+++ book-network-ui/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://13.61.27.122:8080/api/v1'
+  apiUrl: 'http://13.61.27.122:8088/api/v1'
 };
